64c89da0cf7f2e80d559ec8459c8ed943dd8bb7b
rajkumarsingh07 <rajkumarsingh07@gmail.com>
2022-08-18 13:30:04 +0530

    Create firstprogram.py

---
+++ Automation/Python/firstprogram.py
@@ -0,0 +1,6 @@
+#1st Method
+print("Hello World, This is my 1st program in python!!")
+
+#2nd method using variable
+input_value =  input("Enter the value which you want as a output: ")
+print(input_value)
